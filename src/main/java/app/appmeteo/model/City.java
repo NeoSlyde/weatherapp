c9f15f4da1e46244cb940fd72c64d0db51193841
Diego Imbert <imbert.diego05000@gmail.com>
2021-02-06 19:43:32 +0100

    added City::toString, fixed name visibility

--- src/main/java/app/appmeteo/model/City.java
+++ src/main/java/app/appmeteo/model/City.java
@@ -1,16 +1,20 @@
 package app.appmeteo.model;
 
 public class City {
-
-    String name;
+    private String name;
 
     public City(String name) {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
     public boolean equals(Object o) {
         if (o == null) return false;
-        if (o.getClass() != this.getClass()) return false;
+        if (o.getClass() != City.class) return false;
         return this.name.equalsIgnoreCase(((City)o).name);
     }
 
