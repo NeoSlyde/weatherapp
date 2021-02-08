cdba73cc2d39d54d2bc02ae72d8656f6ea75596d
Diego Imbert <imbert.diego05000@gmail.com>
2021-02-08 18:17:41 +0100

    Reformatted appMeteoCLI

--- src/main/java/app/appmeteo/AppMeteoCLI.java
+++ src/main/java/app/appmeteo/AppMeteoCLI.java
@@ -14,25 +14,27 @@ public class AppMeteoCLI {
         /*
          * Input sanitizer
          */
-        if (args.length != 2) {
+        if (args.length != 1 && args.length != 2) {
             System.out.println("Erreur: Nombre d'arguments invalides");
             System.out.println("Argument 1: Ville");
-            System.out.println("Argument 2: Date (jj/MM/aaaa)");
+            System.out.println("Argument 2 (Optional): Date (jj/MM/aaaa)");
             System.exit(-1);
         }
 
-        String date = args[1];
-        int day = 0, month = 0, year = 0;
-        try {
-            day = Integer.parseInt(date.substring(0, 2));
-            month = Integer.parseInt(date.substring(3, 5));
-            year = Integer.parseInt(date.substring(6, 10));
-            if (day < 1 || day > 31 || month < 1 || month > 12)
-                throw new IllegalArgumentException();
-        } catch (IllegalArgumentException e) {
-            System.out.println("Erreur: Date invalide");
-            System.out.println("Format: jj/MM/aaaa");
-            System.exit(-1);
+        Optional<LocalDate> date = Optional.empty();
+        if (args.length == 2) {
+            try {
+                int day = Integer.parseInt(args[1].substring(0, 2));
+                int month = Integer.parseInt(args[1].substring(3, 5));
+                int year = Integer.parseInt(args[1].substring(6, 10));
+                if (day < 1 || day > 31 || month < 1 || month > 12)
+                    throw new IllegalArgumentException();
+                date = Optional.of(LocalDate.of(year, month, day));
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erreur: Date invalide");
+                System.out.println("Format: jj/MM/aaaa");
+                System.exit(-1);
+            }
         }
 
         /*
@@ -44,7 +46,7 @@ public class AppMeteoCLI {
 
         System.out.println("Voici la météo à " + args[0] + " le " + args[1] + ":");
 
-        Optional<MultiTempWeather> weather = MultiTempWeather.getWeather(weatherList, LocalDate.of(year, month, day));
+        Optional<MultiTempWeather> weather = MultiTempWeather.getWeather(weatherList, date.get());
 
         weather.ifPresentOrElse((w) -> {
             System.out.printf(" - Matin:   %.0f°C\n", w.morningTemperature.toCelcius());
