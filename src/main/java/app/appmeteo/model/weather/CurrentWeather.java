ad3184d93e8ed8677c9c42bb71ce25aa5819cbdb
Diego Imbert <imbert.diego05000@gmail.com>
2021-02-08 16:36:11 +0100

    Added currentWeather

---
+++ src/main/java/app/appmeteo/model/weather/CurrentWeather.java
@@ -0,0 +1,9 @@
+package app.appmeteo.model.weather;
+
+public class CurrentWeather extends Weather {
+    public CurrentWeather(Weather weather, Temperature temperature) {
+        super(weather);
+        this.temperature = temperature;
+    }
+    public final Temperature temperature;
+}
