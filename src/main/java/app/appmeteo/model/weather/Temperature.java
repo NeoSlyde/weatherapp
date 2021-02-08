5b444d832fff43ca5046d1b7f0435e745ff0c40f
Diego Imbert <imbert.diego05000@gmail.com>
2021-02-08 15:32:02 +0100

    removed Temperature isKelvin

--- src/main/java/app/appmeteo/model/weather/Temperature.java
+++ src/main/java/app/appmeteo/model/weather/Temperature.java
@@ -2,24 +2,16 @@ package app.appmeteo.model.weather;
 
 public class Temperature {
     private final double temperature;
-    private boolean isKelvins;
 
     public Temperature(int kelvins) {
         this.temperature = kelvins;
-        this.isKelvins = true;
     }
 
     public double toKelvins() {
-        if(isKelvins){
-            return temperature;
-        }
-        return temperature - 273.15;
+        return temperature;
     }
     public double toCelcius() {
-        if(!isKelvins){
-            return temperature + 273.15;
-        }
-        return temperature;
+        return temperature - 273.15;
     }
 
 }
