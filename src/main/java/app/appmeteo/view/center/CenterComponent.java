e97239448e6bf4d68e4e53c1493a90c553fcb665
Diego Imbert <imbert.diego05000@gmail.com>
2021-02-22 20:43:24 +0100

    Added city setter

--- src/main/java/app/appmeteo/view/center/CenterComponent.java
+++ src/main/java/app/appmeteo/view/center/CenterComponent.java
@@ -1,13 +1,19 @@
 package app.appmeteo.view.center;
 
+import app.appmeteo.model.City;
 import app.appmeteo.view.misc.AppLabel;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 
 public class CenterComponent extends VBox {
-    AppLabel cityLabel = new AppLabel("Rennes", "city-label");
-    AppLabel cityWatermark = new AppLabel("Rennes", "city-watermark");
+    AppLabel cityLabel = new AppLabel("", "city-label");
+    AppLabel cityWatermark = new AppLabel("", "city-watermark");
+
+    void setCity(City city) {
+        cityLabel.setText(city.toString());
+        cityWatermark.setText(city.toString());
+    }
 
     public CenterComponent(Scene scene) {
         this.getChildren().add(new DateSelectorComponent(scene));
@@ -15,6 +21,7 @@ public class CenterComponent extends VBox {
         Pane pane = new Pane();
         pane.getChildren().add(cityLabel);
         cityLabel.setPadding(new Insets(25, 0, 0, 50));
+        setCity(new City("Rennes")); //! Temporary
         pane.getChildren().add(cityWatermark);
         // Center horizontally
         cityWatermark.layoutXProperty()
