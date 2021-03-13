5a726211b20b1d8574728364b4a4a11b7109ae93
Bassem MSAYIF <bassem.msayif@etu.univ-amu.fr>
2021-03-13 15:26:31 +0100

    Added Close and minimize buttons and ability to move the window with TopBarComponent and removed the Window Bar.

--- src/main/java/app/appmeteo/AppMeteo.java
+++ src/main/java/app/appmeteo/AppMeteo.java
@@ -5,6 +5,7 @@ import app.appmeteo.view.AppScene;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class AppMeteo extends Application {
@@ -16,6 +17,7 @@ public class AppMeteo extends Application {
             getClass().getResource("/css/style.css").toExternalForm(),
             getClass().getResource("/css/center.css").toExternalForm()
         );
+        window.initStyle(StageStyle.TRANSPARENT);
         window.setTitle("Project Z: AppMeteo");
         window.setScene(scene);
         window.setResizable(false);
--- src/main/java/app/appmeteo/view/topBar/TopBarComponent.java
+++ src/main/java/app/appmeteo/view/topBar/TopBarComponent.java
@@ -8,14 +8,19 @@ import app.appmeteo.view.AppScene;
 import app.appmeteo.view.center.CenterComponent;
 import app.appmeteo.view.center.PartOfTheDayComponent;
 import app.appmeteo.view.misc.*;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.geometry.*;
 import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -26,15 +31,32 @@ public class TopBarComponent extends HBox{
 
     public HBox middleSection;
     private Label meteoLogo = new AppLabel("Meteo", "title");
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public TopBarComponent(Scene scene, AppScene appScene) {
         middleSection = new MiddleSection(scene, appScene);
         meteoLogo.setPadding(new Insets(0, 0, 0, 12));
-        this.getChildren().addAll(meteoLogo, middleSection);
+        HBox rightSection = new RightSection(scene,appScene);
+        this.getChildren().addAll(meteoLogo, middleSection, rightSection);
         this.setSpacing(276);
         this.setPadding(new Insets(0, 0, 0, 20));
         this.setBackground(new Background(new BackgroundFill(Color.rgb(55, 55, 55), CornerRadii.EMPTY, Insets.EMPTY)));
         this.setPrefHeight(70);
+
+        this.setOnMousePressed(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                appScene.getWindow().setX(event.getScreenX() - xOffset);
+                appScene.getWindow().setY(event.getScreenY() - yOffset);
+            }
+        });
+
     }
 
     private class MiddleSection extends HBox {
@@ -80,4 +102,23 @@ public class TopBarComponent extends HBox{
             });
         }
     }
+
+    private class RightSection extends HBox {
+        RightSection(Scene scene, AppScene appScene) {
+            this.getChildren().addAll(new ReduceButton(scene, ()->{
+                Stage obj = (Stage) this.getScene().getWindow();
+                obj.setIconified(true);
+            }, 0.5));
+
+            this.getChildren().addAll(new CloseButton(scene, ()->{
+                Platform.exit();
+                System.exit(0);
+            }, 0.5));
+
+            this.setSpacing(15);
+            this.setPadding(new Insets(10, 0, 0, 15));
+
+        }
+    }
+
 }
